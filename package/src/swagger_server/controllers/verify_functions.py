2eb63fcca8565029051742376d5699c9333d43cb
Sabine <sabine.bertram@mailbox.org>
2020-03-20 10:02:14 +0100

    fix: expect different data format

--- package/src/swagger_server/controllers/verify_functions.py
+++ package/src/swagger_server/controllers/verify_functions.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 def _verify_degree(filename, student_id, degree_code):
-  data = pd.read_csv(filename, dtype = {'student id': 'string', 'degree code': 'string', 'complete': 'bool'})
-  data_filtered = data[(data['student id'] == student_id) & (data['degree code'] == degree_code)]
+  data = pd.read_csv(filename, header=None, names=['student_id', 'degree_code'], dtype = {'student_id': 'string', 'degree_code': 'string'})
+  data_filtered = data[(data['student_id'] == student_id) & (data['degree_code'] == degree_code)]
   if data_filtered.empty:
     return False
   else:
-    return bool(data_filtered.iloc[0]['complete'])
+    return True
--- package/test/test_verify_functions.py
+++ package/test/test_verify_functions.py
@@ -2,6 +2,5 @@ from src.swagger_server.controllers.verify_functions import _verify_degree
 
 def test_verify_degree():
   filename = './mockdata/sb-mock-data.csv'
-  assert(_verify_degree(filename, '56434247', 'CB002') == True)
-  assert(_verify_degree(filename, '60466575', 'CB001') == False)
-  assert(_verify_degree(filename, '87944512', 'CB002') == False)
+  assert(_verify_degree(filename, '60466575', 'CB001') == True)
+  assert(_verify_degree(filename, '56434247', 'CB001') == False)
